1c393601b9730f4eb2c835188a64115d19fc63a4
易泽 <yize@rrcs-172-254-225-206.nyc.biz.rr.com>
2019-04-19 15:55:23 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        初始化
+      </div>
+    );
+  }
+}
+
+export default App;
