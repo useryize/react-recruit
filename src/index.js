e28917342713b194900a45c8e4d76d0e038a8c12
YIZE <yize@yizedeMacBook-Pro.local>
2019-04-21 18:03:04 +0800

    redex调试工具

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import App from './App';
 import { reducer, addGun, removeGun, addGunAsync } from './reducer';
  
+// 按使用compose把中间件与redux调试工具链接
 const store = createStore(reducer, compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
